1d4aa6999821200a9b667e25e77721e3304e2a97
mack-code0 <macdon202@gmail.com>
2025-02-02 18:02:32 +0100

    Layout

--- src/pages/Dashboard/components/StockChart.tsx
+++ src/pages/Dashboard/components/StockChart.tsx
@@ -36,10 +36,10 @@ const InteractiveChart = () => {
   })
 
   return (<>
-    <div className="flex items-center justify-between mb-3">
+    <div className="flex flex-col sm:flex-row space-y-3 sm:space-y-0 sm:items-center justify-between mb-3">
       <h1 className="text-[18px] text-primText font-[500]">Balance</h1>
 
-      <div className="flex border border-[#66666660] rounded-[6px] overflow-hidden">
+      <div className="flex border border-[#66666660] rounded-[6px] overflow-hidden w-fit">
         {[
           "1D",
           "1W",
@@ -53,31 +53,33 @@ const InteractiveChart = () => {
             className={`${state.activeFilter === btn && "text-primText/100 bg-[#66666660]"} text-[12px] px-4 py-1 text-primText/60`} key={idx}>{btn}</button>)}
       </div>
     </div>
-    <div style={{ width: "100%", height: "250px" }}>
-      <ResponsiveContainer width="100%" height="100%">
-        <AreaChart
-          data={data}
-          margin={{ top: 20, right: 30, left: 0, bottom: 0 }}
-        >
-          <defs>
-            <linearGradient id="colorBalance" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#34D399" stopOpacity={0.3} />
-              <stop offset="95%" stopColor="#34D399" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <XAxis tickLine={false} axisLine={false} dataKey="month" tick={{ fill: "#808080", fontSize: 12 }} dy={10} />
-          <YAxis tickFormatter={val => `$${val > 0 ? formatter.format(val) : val}`} tickLine={false} axisLine={false} tick={{ fill: "#808080", fontSize: 12 }} />
-          <CartesianGrid strokeWidth={.5} vertical={false} stroke="#66666660" />
-          <Tooltip contentStyle={{ background: "#18181B", border: "none", borderRadius: "8px", color: "#fff" }} />
-          <Area
-            type="monotone"
-            dataKey="balance"
-            stroke="#34D399"
-            fillOpacity={1}
-            fill="url(#colorBalance)"
-          />
-        </AreaChart>
-      </ResponsiveContainer>
+    <div className="w-full overflow-x-scroll no-scrollbar">
+      <div className="h-[250px] w-[900px] sm:w-full">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            data={data}
+            margin={{ top: 20, right: 30, left: 0, bottom: 0 }}
+          >
+            <defs>
+              <linearGradient id="colorBalance" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#34D399" stopOpacity={0.3} />
+                <stop offset="95%" stopColor="#34D399" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis tickLine={false} axisLine={false} dataKey="month" tick={{ fill: "#808080", fontSize: 12 }} dy={10} />
+            <YAxis tickFormatter={val => `$${val > 0 ? formatter.format(val) : val}`} tickLine={false} axisLine={false} tick={{ fill: "#808080", fontSize: 12 }} />
+            <CartesianGrid strokeWidth={.5} vertical={false} stroke="#66666660" />
+            <Tooltip contentStyle={{ background: "#18181B", border: "none", borderRadius: "8px", color: "#fff" }} />
+            <Area
+              type="monotone"
+              dataKey="balance"
+              stroke="#34D399"
+              fillOpacity={1}
+              fill="url(#colorBalance)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   </>
   );
